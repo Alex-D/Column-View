9c07035b364a0cc2f672e7f0509bc2aa34334eb5
Alex-D <contact@alex-d.fr>
2020-07-02 03:31:49 +0200

    feat: refactor header layout

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import {VNode} from 'snabbdom/src/package/vnode'
 import {throttle} from 'lodash-es'
 import {v4 as uuid} from 'uuid'
 
+const DEFAULT_DISPLAY_MODE = 'single-page'
 const DEFAULT_COLUMNS_COUNT = 4
 const MIN_COLUMNS_COUNT = 1
 const MAX_COLUMNS_COUNT = 10
@@ -13,7 +14,10 @@ const DEFAULT_COLUMNS_WIDTH = 420
 const MIN_COLUMNS_WIDTH = 320
 const MAX_COLUMNS_WIDTH = 4000
 
+type DisplayMode = 'single-page' | 'multi-page'
+
 interface State {
+	displayMode: DisplayMode,
 	url: string,
 	columnsCount: number,
 	columnsWidth: number,
@@ -35,6 +39,7 @@ const getUrlSearchParam = (key: string): string => {
 	return urlSearchParams.get(key) || ''
 }
 
+const displayMode: DisplayMode = getUrlSearchParam('displayMode') as DisplayMode || DEFAULT_DISPLAY_MODE
 const url: string = getUrlSearchParam('url')
 const columnsCount: number = parseInt(getUrlSearchParam('columnsCount')) || DEFAULT_COLUMNS_COUNT
 const columnsWidth: number = parseInt(getUrlSearchParam('columnsWidth')) || DEFAULT_COLUMNS_WIDTH
@@ -42,6 +47,7 @@ const hasNavColumn = true
 const app: HTMLElement = document.getElementById('app') as HTMLElement
 
 const state: State = {
+	displayMode,
 	url,
 	columnsCount,
 	columnsWidth,
@@ -94,6 +100,7 @@ function scroll(columnIndex: number): void {
 function updateHistory(state: State): void {
 	const baseUrl = window.location.origin + window.location.pathname
 	const searchParams = new URLSearchParams()
+	searchParams.set('displayMode', state.displayMode)
 	searchParams.set('url', state.url)
 	searchParams.set('columnsCount', state.columnsCount.toString())
 	searchParams.set('columnsWidth', state.columnsWidth.toString())
@@ -121,6 +128,12 @@ const onColumnsWidthChange = throttle((e: Event): void => {
 	updateHistory(state)
 	render(state)
 }, 100)
+
+const onDisplayModeChange = (e: Event): void => {
+	const event = e as DOMEvent<HTMLInputElement>
+	state.displayMode = event.target.value as DisplayMode
+	updateHistory(state)
+}
 
 const onIframeLoad = (e: Event): void => {
 	const iframe = e.target as HTMLIFrameElement
@@ -163,35 +176,100 @@ function view(state: State): VNode {
 		columns.unshift(
 			h('div.column.nav-column',
 				[
-					h('div.nav-column--container', {},[
+					h('div.nav-column--container', [
 						h(`iframe`, {attrs: {src: state.url, frameborder: '0', scrolling: 'no'}}),
 					]),
 				],
-			)
+			),
 		)
 	}
 
 	return h('div#app', {attrs: {style: `--columns-count: ${state.columnsCount}; --columns-width: ${state.columnsWidth}px`}}, [
-		h('div.options', [
-			h('div.field', [
-				h('input', {attrs: {type: 'url', name: 'url', id: 'url', value: state.url}, on: {change: onUrlChange}}),
-				h('input', {
-					attrs: {type: 'number', name: 'columnsWidth', id: 'columnsWidth', min: MIN_COLUMNS_WIDTH, max: MAX_COLUMNS_WIDTH, value: state.columnsWidth},
-					on: {change: onColumnsWidthChange},
-				}),
+		h('header', [
+			h('h1.header-block.header-logo', 'Column View'),
+			h('div.header-block', [
+				h('div.header-block--icon', []),
+				h('div.header-block--content', [
+					h('div.header-block--title', 'Type'),
+					h('div.header-switch', [
+						h('input.header-switch--radio', {
+							attrs: {
+								id: 'switch-single-page',
+								type: 'radio',
+								name: 'display-mode',
+								value: 'single-page',
+								checked: state.displayMode === 'single-page',
+							},
+							on: {
+								change: onDisplayModeChange,
+							},
+						}),
+						h('label.header-switch--item', {attrs: {for: 'switch-single-page'}},  [
+							'Single page',
+						]),
+						h('input.header-switch--radio', {
+							attrs: {
+								id: 'switch-multi-page',
+								type: 'radio',
+								name: 'display-mode',
+								value: 'multi-page',
+								checked: state.displayMode === 'multi-page',
+							},
+							on: {
+								change: onDisplayModeChange,
+							},
+						}),
+						h('label.header-switch--item', {attrs: {for: 'switch-multi-page'}}, [
+							'Multi page',
+						]),
+					]),
+				]),
 			]),
-			h('div.field', [
-				h('button', {
-					// @ts-ignore: Wrong typing in Snabbdom lib
-					on: {click: [setColumnsCount, state.columnsCount + 1]},
-					attrs: {disabled: state.columnsCount >= MAX_COLUMNS_COUNT},
-				}, '+'),
-				h('span.column-count', state.columnsCount),
-				h('button', {
-					// @ts-ignore: Wrong typing in Snabbdom lib
-					on: {click: [setColumnsCount, state.columnsCount - 1]},
-					attrs: {disabled: state.columnsCount <= MIN_COLUMNS_COUNT},
-				}, '-'),
+			h('div.header-block', [
+				h('div.header-block--icon', []),
+				h('div.header-block--content', [
+					h('label.header-block--title', {
+						attrs: {for: 'url'},
+					}, 'URL'),
+					h('input', {
+						attrs: {type: 'url', name: 'url', id: 'url', value: state.url},
+						on: {change: onUrlChange},
+					}),
+				]),
+			]),
+			h('div.header-block', [
+				h('div.header-block--icon', []),
+				h('div.header-block--content', [
+					h('div.header-block--title', 'Column count'),
+					h('button', {
+						// @ts-ignore: Wrong typing in Snabbdom lib
+						on: {click: [setColumnsCount, state.columnsCount - 1]},
+						attrs: {disabled: state.columnsCount <= MIN_COLUMNS_COUNT},
+					}, '-'),
+					h('span.column-count', state.columnsCount),
+					h('button', {
+						// @ts-ignore: Wrong typing in Snabbdom lib
+						on: {click: [setColumnsCount, state.columnsCount + 1]},
+						attrs: {disabled: state.columnsCount >= MAX_COLUMNS_COUNT},
+					}, '+'),
+				]),
+			]),
+			h('div.header-block', [
+				h('div.header-block--icon', []),
+				h('div.header-block--content', [
+					h('div.header-block--title', 'Screen size'),
+					h('input', {
+						attrs: {
+							type: 'number',
+							name: 'columnsWidth',
+							id: 'columnsWidth',
+							min: MIN_COLUMNS_WIDTH,
+							max: MAX_COLUMNS_WIDTH,
+							value: state.columnsWidth,
+						},
+						on: {change: onColumnsWidthChange},
+					}),
+				]),
 			]),
 		]),
 		h('div.columns', columns),
@@ -205,7 +283,9 @@ function render(state: State) {
 	patch(oldVNode || app, newVNode)
 	oldVNode = newVNode
 
-	scroll(0)
+	setTimeout(() => {
+		scroll(0)
+	})
 }
 
 render(state)
